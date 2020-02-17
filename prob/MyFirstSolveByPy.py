6b6aa65fe1e1a23db1b2a90d86f188c72e6cb0e7
leeyonghoon <leeyonghoon94@gmail.com>
2020-02-17 23:18:30 +0900

    divided folder for Porb

---
+++ prob/MyFirstSolveByPy.py
@@ -0,0 +1,11 @@
+def solution(participant, completion):
+    for i in completion:
+        participant.remove(i)
+    return participant[0]
+
+participant = ['lee','yong','hoon']
+completion = [ 'yong','hoon']
+
+print(solution(participant,completion))
+
+# 효율성 0점임 시간복잡도 고려해서 다시풀기.
