4d1a6b44e055ba28050fc22865b2f6feef121889
leeyonghoon <leeyonghoon94@gmail.com>
2020-02-17 19:53:36 +0900

    add files

---
+++ myFirstFuncInPython.py
@@ -0,0 +1,24 @@
+wow =3
+
+def firstFunc(arg) ->wow:
+    """
+    This function is blah blah
+
+    Aggs :
+        parm1 : balalsjdls
+        parm2 : soifjaowiejfoi
+    
+    Returns :
+        wow (int) : owifoweifo
+    """
+    wow =1
+    print('wow my func!')
+    return wow
+
+#good = firstFunc(wow)
+
+#print(good)
+
+#help(firstFunc)
+
+print(firstFunc.__doc__)
